80d66c50a7eaba79b6b1b813a74e5d7acdccb515
Brian Saltz Jr <brian.saltzjr@gocrisp.com>
2023-12-05 13:21:12 -0500

    Day 5 solution

--- src/main/kotlin/com/github/bsaltz/adventofcode/util/LangUtils.kt
+++ src/main/kotlin/com/github/bsaltz/adventofcode/util/LangUtils.kt
@@ -3,4 +3,24 @@ package com.github.bsaltz.adventofcode.util
 object LangUtils {
     inline fun <T, K, V> Iterable<T>.associateIndexed(transform: (Int, T) -> Pair<K, V>): Map<K, V> =
         mapIndexed(transform).associate { it }
+
+    fun <T> Sequence<T>.zipToPairs(): Sequence<Pair<T, T>> =
+        zipWithNext().filterIndexed { index, _ -> index.and(1) == 0 }
+
+    fun LongRange.intersectRange(other: LongRange): LongRange? {
+        if (step != 1L || other.step != 1L) {
+            TODO("This operation currently only supports ranges with a step value of 1,")
+        }
+        val thisFirst = first
+        val thisLast = last
+        val otherLast = other.last
+        val otherFirst = other.first
+        return if (thisFirst <= otherLast && otherFirst <= thisLast) {
+            maxOf(thisFirst, otherFirst)..minOf(thisLast, otherLast)
+        } else {
+            null
+        }
+    }
+
+    fun LongRange.shiftRange(distance: Long): LongRange = LongRange(start + distance, endInclusive + distance)
 }
---
+++ src/main/kotlin/com/github/bsaltz/adventofcode/y2023/d05/Day5.kt
@@ -0,0 +1,113 @@
+package com.github.bsaltz.adventofcode.y2023.d05
+
+import com.github.bsaltz.adventofcode.util.LangUtils.intersectRange
+import com.github.bsaltz.adventofcode.util.LangUtils.shiftRange
+import com.github.bsaltz.adventofcode.util.LangUtils.zipToPairs
+import com.github.bsaltz.adventofcode.util.Resource
+import com.github.bsaltz.adventofcode.util.toClassPathResource
+import com.github.bsaltz.adventofcode.util.toStringResource
+
+private const val sampleResultP1: Int = 35
+private const val sampleResultP2: Int = 46
+private val sampleResource: Resource = """
+    seeds: 79 14 55 13
+
+    seed-to-soil map:
+    50 98 2
+    52 50 48
+
+    soil-to-fertilizer map:
+    0 15 37
+    37 52 2
+    39 0 15
+
+    fertilizer-to-water map:
+    49 53 8
+    0 11 42
+    42 0 7
+    57 7 4
+
+    water-to-light map:
+    88 18 7
+    18 25 70
+
+    light-to-temperature map:
+    45 77 23
+    81 45 19
+    68 64 13
+
+    temperature-to-humidity map:
+    0 69 1
+    1 0 69
+
+    humidity-to-location map:
+    60 56 37
+    56 93 4
+""".trimIndent().toStringResource()
+
+private val inputResource: Resource = "2023/aoc-2023-d05-input.txt".toClassPathResource()
+
+fun main() {
+    println("# Part 1")
+    println("  Sample result: ${Day5.solutionP1(sampleResource)} (expected: ${sampleResultP1})")
+    println("  Input result: ${Day5.solutionP1(inputResource)}")
+    println("# Part 2")
+    println("  Sample result: ${Day5.solutionP2(sampleResource)} (expected: ${sampleResultP2})")
+    println("  Input result: ${Day5.solutionP2(inputResource)}")
+}
+
+object Day5 {
+
+    fun solutionP1(resource: Resource): Long = solution(resource, ::parseSeedsP1)
+    fun solutionP2(resource: Resource): Long = solution(resource, ::parseSeedsP2)
+
+    private fun solution(resource: Resource, parseSeeds: (String) -> Sequence<LongRange>) =
+        resource.useLines { lines ->
+            lines.fold(Pair(emptySequence<LongRange>(), emptyList<Mapper>())) { pair, line ->
+                val (values, mappers) = pair
+                when {
+                    line.startsWith("seeds: ") -> parseSeeds(line.removePrefix("seeds: ")) to mappers
+                    line.isBlank() && mappers.isNotEmpty() ->
+                        values.flatMap { value ->
+                            val ranges = mappers.mapNotNull { it.map(value) }.takeIf { it.isNotEmpty() }
+                                ?: listOf(IdentityMapper.map(value))
+                            ranges
+                        } to emptyList()
+                    line.endsWith(" map:") -> values to emptyList()
+                    line.isBlank() -> values to mappers
+                    else -> values to mappers + parseRangeMapper(line)
+                }
+            }.first.minOf { it.first }
+        }
+
+    private fun parseSeedsP1(seedsStr: String): Sequence<LongRange> = seedsStr.trim().splitToSequence(" ")
+        .map { it.toLong() }
+        .map { it..it }
+
+    private fun parseSeedsP2(seedsStr: String): Sequence<LongRange> = seedsStr.trim().splitToSequence(" ")
+        .map { it.toLong() }
+        .zipToPairs()
+        .map { it.first..<(it.first + it.second) }
+
+    private fun parseRangeMapper(rangeStr: String): RangeMapper =
+        rangeStr.trim().split(" ").let { (destinationStr, sourceStr, lengthStr) ->
+            RangeMapper(destinationStr.toLong(), sourceStr.toLong(), lengthStr.toLong())
+        }
+
+    private interface Mapper {
+        fun map(input: Long): Long?
+        fun map(inputs: LongRange): LongRange?
+    }
+
+    private object IdentityMapper : Mapper {
+        override fun map(input: Long): Long = input
+        override fun map(inputs: LongRange): LongRange = inputs
+    }
+
+    private data class RangeMapper(val destination: Long, val source: Long, val length: Long) : Mapper {
+        private val range = source..<source + length
+        override fun map(input: Long): Long? = input.takeIf { it in range }?.let { it - source + destination }
+        override fun map(inputs: LongRange): LongRange? =
+            range.intersectRange(inputs)?.shiftRange(destination - source)
+    }
+}
